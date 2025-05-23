6330e5741a6182db6ceb9c39241c66b412cb9e7c
duncanophone <duncan_williams@hotmail.co.uk>
2025-05-23 17:13:53 +0100

    Update tailwind.config.ts

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -3,8 +3,8 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   darkMode: ["class"],
   content: [
-    "./client/index.html",
-    "./client/src/**/*.{js,ts,jsx,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -15,7 +15,7 @@ const config: Config = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
-        // add other color extensions here if needed...
+        // other colors...
       },
     },
   },
